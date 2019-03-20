70596843184531cef58684261d40d72279ee0eac
Michael Allen Hardeman <michael.hardeman@protonmail.com>
2019-03-20 19:35:57 -0400

    fixing the options page.
    it doens't fully work, but it's better than it was.

--- options/js/application.js
+++ options/js/application.js
@@ -1,105 +1,115 @@
-﻿(function(globals) {
+﻿(function (globals) {
   'use strict';
-  
-  if(!angular) {
+
+  if (!globals.angular) {
     throw new Error('AngularJS is required for this module');
   }
-  
+
   globals.constants.APPLICATION_NAME = globals.constants.NAME + 'Options';
-  
-  var application = angular.module(globals.constants.APPLICATION_NAME, ['ngRoute', 'ngResource', 'ui.bootstrap', 'ngStorage', 'ngTwitch', 'ngError']);
-  
+
+  var application = globals.angular.module (globals.constants.APPLICATION_NAME, [
+    'ngRoute',
+    'ngResource',
+    'ui.bootstrap',
+    'ngStorage',
+    'ngTwitch',
+    'ngError'
+  ]);
+
   ///////////////
   // Constants //
   ///////////////
   application.constants = {
-    'ROUTES' : {
-      'OPTIONS' : '/',
-      'ABOUT'   : '/about',
-      'DONATE'  : '/donate',
-      'ERROR'   : '/error'
+    'ROUTES': {
+      'OPTIONS': '/',
+      'ABOUT'  : '/about',
+      'DONATE' : '/donate',
+      'ERROR'  : '/error'
     }
   };
-  
+
   ///////////////////////////
   // Initialize $rootScope //
   ///////////////////////////
-  application.run([
+  application.run ([
     '$rootScope',
     '$localStorage',
-  function(
-    $rootScope,
-    $localStorage
-  ) {
-    globals.scope = $rootScope;
-    $rootScope.storage = $localStorage.$default({
-      username    : '',
-      defaultGame : ''
-    });
-    $rootScope.translate = chrome.i18n.getMessage;
-    $rootScope.messages  = [];
-    
-    $rootScope.port = chrome.runtime.connect({name: globals.constants.APPLICATION_NAME});
-    $rootScope.port.onMessage.addListener(function(messages) {
-      $rootScope.messages = $rootScope.messages.concat(messages);
-      if(!$rootScope.$$phase){
-        $rootScope.$apply();
-      }
-    });
-    
-    $rootScope.$on('$routeChangeStart', function(next, current) {
-      $rootScope.messages.length = 0;
-    });
-  }]);
-  
+    function(
+      $rootScope,
+      $localStorage
+    ) {
+      globals.scope = $rootScope;
+      $rootScope.storage = $localStorage.$default({
+        username: '',
+        defaultGame: ''
+      });
+      $rootScope.translate = globals.chrome.i18n.getMessage;
+      $rootScope.messages = [];
+
+      $rootScope.port = globals.chrome.runtime.connect({
+        name: globals.constants.APPLICATION_NAME
+      });
+      $rootScope.port.onMessage.addListener(function(messages) {
+        $rootScope.messages = $rootScope.messages.concat(messages);
+        if (!$rootScope.$$phase) {
+          $rootScope.$apply();
+        }
+      });
+
+      $rootScope.$on('$routeChangeStart', function () { // args next, current
+        $rootScope.messages.length = 0;
+      });
+    }
+  ]);
+
   //////////////////////
   // Configure Routes //
   //////////////////////
-  application.config([ '$routeProvider', function($routeProvider) {
-    
-    $routeProvider.when( application.constants.ROUTES.OPTIONS, {
-      templateUrl : 'views/options.html',
-      controller  : 'OptionsController'
+  application.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider.when (application.constants.ROUTES.OPTIONS, {
+      templateUrl: 'views/options.html',
+      controller: 'OptionsController'
     });
-    
-    $routeProvider.when( application.constants.ROUTES.ABOUT, {
-      templateUrl : 'views/about.html',
-      controller  : 'AboutController'
+
+    $routeProvider.when (application.constants.ROUTES.ABOUT, {
+      templateUrl: 'views/about.html',
+      controller: 'AboutController'
     });
-    
-    $routeProvider.when( application.constants.ROUTES.DONATE, {
-      templateUrl : 'views/donate.html',
-      controller  : 'DonateController'
+
+    $routeProvider.when (application.constants.ROUTES.DONATE, {
+      templateUrl: 'views/donate.html',
+      controller: 'DonateController'
     });
-    
-    $routeProvider.when( application.constants.ROUTES.ERROR, {
-      templateUrl : 'views/error.html',
-      controller  : 'ErrorController'
+
+    $routeProvider.when (application.constants.ROUTES.ERROR, {
+      templateUrl: 'views/error.html',
+      controller: 'ErrorController'
     });
-    
-    $routeProvider.otherwise({
-      redirectTo : application.constants.ROUTES.OPTIONS
+
+    $routeProvider.otherwise ({
+      redirectTo: application.constants.ROUTES.OPTIONS
     });
   }]);
-  
+
   //////////////////////////////////
   // Configure $http Interceptors //
   //////////////////////////////////
-  application.config([ '$provide', '$httpProvider', function($provide, $httpProvider) {
-    $provide.factory('HttpInterceptor', ['$q', function ($q) {
+  application.config (['$provide', '$httpProvider', function ($provide, $httpProvider) {
+    $provide.factory ('HttpInterceptor', ['$q', function ($q) {
       var publicApi = {
-        'request'       : undefined,
-        'requestError'  : undefined,
-        'response'      : undefined,
-        'responseError' : undefined
+        'request'      : undefined,
+        'requestError' : undefined,
+        'response'     : undefined,
+        'responseError': undefined
       };
-      
+
       function requestSuccess (config) {
-        // TODO 
+        // TODO:
         return config || $q.when(config);
       }
- 
-      function requestError(rejection) {
+
+      function requestError (rejection) {
         // TODO
         // if (canRecover(rejection)) {
         //   return responseOrNewPromise
@@ -107,36 +117,35 @@
         return $q.reject(rejection);
       }
 
-      function responseSuccess(response) {
+      function responseSuccess (response) {
         // TODO
         return response || $q.when(response);
       }
 
-      function responseError(rejection) {
+      function responseError (rejection) {
         // TODO
         // if (canRecover(rejection)) {
         //   return responseOrNewPromise
         // }
-        return $q.reject(rejection);
+        return $q.reject (rejection);
       }
-      
+
       publicApi.request       = requestSuccess;
       publicApi.requestError  = requestError;
       publicApi.response      = responseSuccess;
       publicApi.responseError = responseError;
-      
+
       return publicApi;
     }]);
-    
+
     $httpProvider.interceptors.push('HttpInterceptor');
   }]);
-  
+
   /////////////////////////////////
   // Configure link sanitization //
   /////////////////////////////////
-  application.config([ '$compileProvider', function( $compileProvider ) {
+  application.config (['$compileProvider', function ($compileProvider) {
     $compileProvider.aHrefSanitizationWhitelist (/^\s*(https?|ftp|mailto|file|tel|chrome-extension):/);
   }]);
-  
+
 }(this));
-  
